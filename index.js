12fb212cb208ca87f8481d35a05025f6d48deb17
Atte Lassila <atte.lassila@cs.helsinki.fi>
2016-12-16 18:40:44 +0200

    Initial commit with the initial server setup

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const request = require('request');
+const port = 3000;
+
+
+const listoptions = {
+    uri: 'restaurants/',
+    baseUrl: 'http://hyy-lounastyokalu-production.herokuapp.com/publicapi/',
+    method: 'GET',
+    json: true
+};
+
+app.get('/restaurants', (req, resp) => {
+    request(listoptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            resp.send(body);
+        }
+    });
+});
+
+app.get('/restaurant/{id}', (req, resp) => {
+    request(listoptions, function (error, response, body) {
+        if (!error && response.statusCode === 200) {
+            resp.send(body);
+        }
+    });
+});
+
+app.listen(30000);
