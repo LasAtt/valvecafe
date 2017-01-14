beb4c27184023b96cc3344a7cfd57486e359575b
Atte Lassila <atte.lassila@cs.helsinki.fi>
2017-01-14 21:29:12 +0200

    Added missing file

---
+++ routes/restaurants/restaurantmenus.js
@@ -0,0 +1,6 @@
+module.exports = function(req, res) {
+    req.collection.find({}, {sort: {_id: 1}, fields: {_id:1, name: 1}})
+              .then(function(result) {
+                res.send(result);
+              });
+}
