fec60d283fbd5bfe6c9f981c0774d7210ee83573
Atte Lassila <atte.lassila@cs.helsinki.fi>
2017-01-15 02:35:53 +0200

    testing jenkins conf

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 var router = require('./routes/index.js');
 
 console.log(process.env.MONGO_PORT_27017_TCP_ADDR+':'+process.env.MONGO_PORT_27017_TCP_PORT);
-var db = monk(process.env.MONGO_PORT_27017_TCP_ADDR+':'+process.env.MONGO_PORT_27017_TCP_PORT+'/kumpulamenu');
+var db = monk('localhost:27017/kumpulamenu');
 
 var port = 3000;
 
