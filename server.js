03a8ec848a04a3f1b0cd3fd0ca1d5787dcfc3765
Atte Lassila <atte.lassila@cs.helsinki.fi>
2017-01-20 21:13:03 +0200

    kumpulamenu->valvecafe

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var express = require('express')
 var app = express();
 var router = require('./routes/index.js');
 
-var db = monk(process.env.MONGO_PORT_27017_TCP_ADDR+':'+process.env.MONGO_PORT_27017_TCP_PORT+'/kumpulamenu');
+var db = monk(process.env.MONGO_PORT_27017_TCP_ADDR+':'+process.env.MONGO_PORT_27017_TCP_PORT+'/valvecafe');
 
 var port = 3000;
 
@@ -17,7 +17,6 @@ app.use(function (req, res, next) {
     req.visibility = {_id: 1, name: 1, areacode: 1};
     next();
 });
-
 
 app.use('/api', router);
 
